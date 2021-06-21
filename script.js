3b7b38ad49b093bd65e5e494b898fe36cea0a057
Whiting <whitinghtennis@gmail.com>
2021-06-21 14:09:44 -0700

    added smiley emojis to title

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+
+
+
+const boomClick = document.getElementById('header');
+boomClick.addEventListener('click', function() {
+    alert('BOOM!');
+});
+
+
+
+
